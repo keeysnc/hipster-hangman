f43538736e50e872eed9427c02c71f9dc567e2d1
Nathan Keeyss <keeysnc@gmail.com>
2019-11-30 23:23:07 -0500

    add materialize for reset button

--- assets/js/game.js
+++ assets/js/game.js
@@ -36,7 +36,7 @@ var app = (function() {
     
 
     var resetBtn = document.createElement('button');
-    resetBtn.setAttribute('class','resetBtn');
+    resetBtn.setAttribute('class','waves-effect waves-light btn');
     resetBtn.style.display = "none";
     resetBtn.innerHTML = "Reset";
     root.appendChild(resetBtn);
@@ -70,7 +70,7 @@ var app = (function() {
     //loop through array and hide all elements
     function init(){
         //setup scoreboard
-        scoreboard.innerHTML = "<p>Guesses: " + guesses + "<p>"
+        scoreboard.innerHTML = "<h4>Guesses: " + '<b>' + guesses + '</b>' + "<h4>"
         
         //render question
         questionContainer.innerHTML = '<h1>' + questionAnswerObj[questionSelect].question + '</h1>';
@@ -133,7 +133,7 @@ var app = (function() {
 
     function updates(){
         if(guesses === 0){
-            scoreboard.innerHTML = "<p>Guesses: " + "Sorry you've lost" + "<p>"
+            scoreboard.innerHTML = "<h4>Guesses: <b>Sorry you've lost</b> </h4>"
             answerContainer.innerHTML = '<h3>' + questionAnswerObj[questionSelect].answer + '</h3>'
             
             resetBtn.style.display = "block";
@@ -142,13 +142,13 @@ var app = (function() {
             })  
             return;
         }else if(!hideArr.includes("_") ){
-                scoreboard.innerHTML = "<p>Guesses: " + "You Win!" + "<p>";
+                scoreboard.innerHTML = "<h4><b>You Win!</b> </h4>"
                 resetBtn.style.display = "block";
                 resetBtn.addEventListener('click',function(){
                 window.location.reload(true);
             })
         }else{
-            scoreboard.innerHTML = "<p>Guesses: " + guesses + "<p>"
+            scoreboard.innerHTML = "<h4>Guesses: " + '<b>' + guesses + '</b>' + "<h4>"
             usedkeyContainer.innerHTML = '<h3>' + wrongGuesses + '</h3>'
         }
     }
@@ -160,114 +160,4 @@ var app = (function() {
 
     render();
 
-    // function letterSplit(word){
-    //     let wordSplit = word.split("");
-    //     let newLetterArr = []
-    //     newLetterArr = wordSplit;
-    //     return newLetterArr;
-    // }
-
-    // function removeBtn(){
-    //     gameBtn.style.display = "none";
-    // }
-
-    // //UPDATE LETTER BANK
-    // function updateWord(answerLetters,hiddenAnswer){
-    //     document.addEventListener('keyup', function(event){
-    //         const referenceArr = answerLetters;  
-    //         let charIndexArr = [];
-    //         let charIndex;
-    //         userKey = event.key;
-    //         //loop through reference array
-    //         //foreach element in the array run an if conditional
-    //         //if userkey is equal to the indexed element 
-    //         //get that all instances of that elements index number and push it to a new array
-    //         referenceArr.forEach(function(element) {
-    //             element.toUpperCase();
-    //             if(userKey === element){
-    //                 charIndex = referenceArr.indexOf(element);
-    //                 // while (charIndex != -1) {
-    //                 //     charIndexArr.push(charIndex);
-    //                 //     charIndex = referenceArr.indexOf(element, charIndex + 1);
-    //                 // }
-    //                 console.log(charIndexArr);
-    //                 return charIndexArr;        
-    //             }
-    //         });
-    //         //   console.log('characterIndexArray: ' + charIndexArr);
-    //         charIndexArr.forEach(el => {
-    //         updatedAnswer = hiddenAnswer.splice(el,charIndexArr.length,userKey);
-    //         // console.log(updatedAnswer);
-    //         });
-    //     });  
-    // }
-
-    // //HIDE ANSWER
-    // function createHiddenAnswer(answerLetters){
-    //     let hiddenAnswer = [];
-
-    //     answerLetters.forEach(letter => {
-    //         const str = letter.toString();
-    //         const hideLetter = str.replace(/[a-z]/gi,'_');
-    //         hiddenAnswer.push(hideLetter);
-            
-    //     });
-
-    //     updateWord(answerLetters,hiddenAnswer);
-    // }
-
-
-    // //RENDER QUESTION
-    // function renderQuestion(){
-        
-    //     for(var i = 0; i < questionAnswerObj.length-1; i++){
-    //         //question box
-    //         const questionBox = document.createElement('div');
-    //         const question = questionAnswerObj[i].question;
-
-    //         questionBox.innerHTML = '<h3>' + question + '</h3>';
-    //         app.appendChild(questionBox);
-    //         break;
-    //     }
-    // }
-
-    // //RENDER ANSWER
-    // function renderAnswer(){
-        
-    //     for(var i = 0; i < questionAnswerObj.length-1; i++){
-    //         //answer box
-    //         const answerBox = document.createElement('div');
-    //         const answerParagraph = document.createElement('p');
-    //         const answer = questionAnswerObj[i].answer.toUpperCase();
-    //         const letters = letterSplit(answer);
-
-    //         answerParagraph.innerHTML = '<h3>' + createHiddenAnswer(letters) + '</h3>';
-    //         app.appendChild(answerBox);
-    //         answerBox.appendChild(answerParagraph);
-    //         break;
-    //     }
-    // }
-
-
-
-    // //start game 
-    // function startGame(){
-    //     gameBtn.innerHTML = 'Start Game';
-    //     // startBtn.setAttribute('class','btn btn-primary');
-    //     app.appendChild(gameBtn);
-    //     gameBtn.addEventListener("click", function(){
-    //         removeBtn();
-    //         renderQuestion();
-    //         renderAnswer();
-    //     });
-    // }
-
-    // function render(){
-    //     startGame();
-    // }
-
-    // render();
-
-    
-    
 })();
